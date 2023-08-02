3c89db5eb2656bad77c5986860cced387601592e
Sandip Neupane <sandipneupane775@gmail.com>
2023-08-02 13:24:00 +0545

    Passing arguments

--- client/src/components/Example.js
+++ client/src/components/Example.js
@@ -1,14 +1,13 @@
 //rfce
 
 //Adding events to Component
-
 export default function Example() {
-    const shoot = () => {
+    const shoot = (a) => {
         alert("goal");
     };
     return (
     <>
-    <button onClick={shoot}> Take the shot! </button>
+    <button onClick={() => shoot("Goal")}> Take the shot! </button>
     </>
     );
 }
